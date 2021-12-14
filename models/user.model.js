36f315e3e585410e8667cbee96a4bc85eba58899
Luisfdearaujo <90002192+Luisfdearaujo@users.noreply.github.com>
2021-12-14 09:17:56 +0000

    Heroku

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,11 @@ const userSchema = new Schema({
 	password: { type: String, required: true },
 	name: { type: String, required: true },
 	role: { type: String, enum: ["admin", "user"], default: "user" },
-	image: { type: String, default: "https://i.imgur.com/yWHfhiG.png" },
+	image: {
+		type: String,
+		default:
+			"https://images.unsplash.com/photo-1584053774180-06193b6838d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+	},
 	slope: [{ type: Schema.Types.ObjectId, ref: "Slope" }],
 });
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,42 +3,40 @@ const router = express.Router();
 const User = require("../models/user.model");
 const { isAuthenticated } = require("./../middleware/jwt.middleware");
 
-
 // GET /api/users/current  - Get current user info
-router.get('/api/users/current', isAuthenticated, async (req, res, next) => {
-  try {
-    // If the user is authenticated we can access the JWT payload via req.payload
-    // req.payload holds the user info that was encoded in JWT during login.
-  
-    const currentUser = req.payload;
-    const user = await User.findById(currentUser._id);
+router.get("/api/users/current", isAuthenticated, async (req, res, next) => {
+	try {
+		// If the user is authenticated we can access the JWT payload via req.payload
+		// req.payload holds the user info that was encoded in JWT during login.
 
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
-  }
-})
+		const currentUser = req.payload;
+		const user = await User.findById(currentUser._id);
+
+		res.status(200).json(user);
+	} catch (error) {
+		next(error);
+	}
+});
 
 // PUT /api/users/current  - Update the current user
-router.put('/api/users/current', isAuthenticated, async (req, res, next) => {
-  try {
-    // If the user is authenticated we can access the JWT payload via req.payload
-    // req.payload holds the user info that was encoded in JWT during login.
-  
-    const currentUser = req.payload;
-    const { email, name } = req.body;
+router.put("/api/user/current", isAuthenticated, async (req, res, next) => {
+	try {
+		// If the user is authenticated we can access the JWT payload via req.payload
+		// req.payload holds the user info that was encoded in JWT during login.
 
-    const updatedUser = await User.findByIdAndUpdate(
-      currentUser._id,
-      { email, name },
-      { new: true }
-    );
+		const currentUser = req.payload;
+		const { email, name, image } = req.body;
 
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    next(error);
-  }
-})
+		const updatedUser = await User.findByIdAndUpdate(
+			currentUser._id,
+			{ email, name, image },
+			{ new: true }
+		);
 
+		res.status(200).json(updatedUser);
+	} catch (error) {
+		next(error);
+	}
+});
 
 module.exports = router;
